95bde298fdbf3c46f73cf8690b2429b38296c557
nyakiri_margaret <margaret.karanja@student.moringaschool.com>
2022-11-28 08:27:28 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
+function scuberGreetingForFeet(feet){
+let result
+if (feet <=400);
+ {result = 'This is on me!';
+  
+}
+return result
+} 
+function scuberGreetingForFeet(feet) {
+  let result
+   if (feet >=2000) {
+    result = 'i will gladly take your 30 bucks,';
+  }
+  
+   return result 
+  }
+
 function scuberGreetingForFeet(){
-  // Write your code here!
-}
+  let result
+  if (feet > 2500) {
+    result = 'No can do,';
+    return result}
+  }
+  function ternaryCheckCity(city){
+    let result;
+    city ==='NYC'? result= 'ok sounds good':result='No go.';
+    return  result
+    ternaryCheckCity ('NYC');
+    ternaryCheckCity ('Pittsburgh');
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  }
+  function switchOnCharmFromTip (tip) {
+    let result;
+    switch(tip){
+    case'generous,'
+      result ='Thank you so much.';
+      break;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+    case 'not as generous.'
+     result = 'Thank you,';
+     break; 
+  
+    default:
+      return 'Bye,';
+      break;}
+  
+  return result;
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous')
+switchOnCharmFromTip('thanks for everything') 
+ }
+  
+
+
+  
+  
+
+ 
+ 
---
+++ scuberGreetingForFeet.js
@@ -0,0 +1,11 @@
+function scuberGreetingForFeet() {
+
+
+  function ternaryCheckCity() {
+    // Write your code here!
+  }
+
+  function switchOnCharmFromTip() {
+    // Write your code here!
+  }
+}
